0f91e3cf93b1c598288dd30a103507f842721dcd
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-08-04 16:08:30 +0200

    Use Staging endpoint

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,9 @@ import TaxiClient from './taxiClient';
 import createDummyTx from './createDummyTx';
 import { Psbt } from 'liquidjs-lib';
 
-const taxiClient = new TaxiClient('http://localhost:8080');
+const taxiClient = new TaxiClient(
+  'https://z7hfdsdqee.execute-api.eu-west-1.amazonaws.com/dev'
+);
 const TETHER_ASSET_HASH =
   'ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2';
 
